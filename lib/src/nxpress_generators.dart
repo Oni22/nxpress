a3701e96e29b30f4cd353d5d15ec48a95e11f710
Onur Özkan <o.ozkan@berlsoft.de>
2021-08-01 16:03:34 +0200

    changes

--- lib/src/nxpress_generators.dart
+++
@@ -1,5 +0,0 @@
-import 'package:build/build.dart';
-import 'package:nxpress/src/generators/nxpress_generator.dart';
-import 'package:source_gen/source_gen.dart';
-
-Builder nxpressGenerator(BuilderOptions options) => SharedPartBuilder([NxpressGenerator()], "nx");
--- lib/src/nxres/strings/nxstrings.dart
+++
@@ -1,5 +0,0 @@
-import 'package:nxpress/nxpress.dart';
-class NxStrings {	
-static final node = NxString({"en":"hallo","de":"test",});	
-static final node2 = NxString({"en":"hallo","de":"test",});
-}
