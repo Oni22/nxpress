e20547a258bec91cbd2c328595d34304e9543e0a
Onur <onur@simpletechs.net>
2021-08-21 12:12:38 +0200

    first version alpa

--- bin/str.dart
+++ bin/str.dart
@@ -1,4 +1,4 @@
-import 'package:nxpress/src/creator/nxpress_creator.dart';
+import 'package:nxpress/src/tools/nxpress_creator.dart';
 import 'package:nxpress/src/models/nxpress_schema.dart';
 
 main(List<String> args) async {
--- lib/nxpress.dart
+++ lib/nxpress.dart
@@ -1,2 +1,4 @@
 library nxpress;
 export 'package:nxpress/src/resource_types/nxstring.dart';
+export 'package:nxpress/src/models/nxpress_resource.dart';
+export 'package:nxpress/src/tools/nxpress_creator.dart';
